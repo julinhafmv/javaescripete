fd8b39b4b5f1999909c9327f496321496f6394c8
julinhafmv <128698307+julinhafmv@users.noreply.github.com>
2023-09-05 12:01:38 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,20 @@
+var num1=1;
+var num2=2;
+
+//console.log (num1+num2);
+
+var soma;
+var resultado;
+
+function calcularSoma(num1 , num2) {
+ soma = num1 + num2 
+  return soma;
+}
+resultado = calcularSoma (num1 , num2);
+console.log('a soma é' ,resultado);
+
+num1 = 30
+num2 = 50
+
+resultado = calcularSoma(num1,num2);
+console.log('a soma é:' ,resultado);
